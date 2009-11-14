e2cc7c6305131c14da9a4dc6b59d849b8a45bb05
david.jimenez19 <david.jimenez19@487b7dc8-a516-11de-afd7-c7ca60a6e2ec>
2009-11-14 19:30:17 +0000

    el script de backup funciona

--- trunk/scripts/backup_mysql.sh
+++ trunk/scripts/backup_mysql.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-export FILE=%2
-export BBDD=%1
+export FILE=$2
+export BBDD=$1
 echo Backup of $BBDD in path $FILE
-mysqldump --add-drop-database -u root $BBDD > FILE
+mysqldump --add-drop-database -u root $BBDD > $FILE
